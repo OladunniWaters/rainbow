ca33910abb41ed9b57c6832cb6cfee4aac95b549
oladunniwaters <waters.oladunni@gmail.com>
2023-08-03 02:28:49 +0100

    hero

--- src/components/pages/landingpage/Hero.jsx
+++ src/components/pages/landingpage/Hero.jsx
@@ -1,5 +1,5 @@
 import './Hero.scss';
-
+import ReactPlayer from 'react-player';
 import hero1 from '../../../assets/Untitled_1.webm';
 
 
@@ -8,7 +8,11 @@ function Hero() {
   return (
     <div className="Hero" >
        <div className='section-1-img-cont'>
-            <img src={hero1} alt='hero-video' className='hero-video'/>
+            <video autoPlay="autoplay" loop="loop" muted>
+                <source src={hero1} type="video/webm" />
+                Sorry, your browser doesn't support videos.
+            </video>
+
         </div>
         <div className='section-1-text-cont'>
             <h3 className='section-1-h3'>ORDER DURING THE SUMMER & SAVE BIG</h3>
