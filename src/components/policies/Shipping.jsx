c4471dd0fc6b0c471bf3367409919350bcbdad32
oladunniwaters <waters.oladunni@gmail.com>
2023-08-07 13:10:13 +0100

    policies responsiveness

--- src/components/policies/Shipping.jsx
+++ src/components/policies/Shipping.jsx
@@ -4,7 +4,7 @@ function Shipping() {
   return (
      <div className='shipping-cont'>
          <h2 className='shipping-header'>Shipping Policy</h2>
-         <p className='shipping-p'>Shipping takes 4-12 days to arrive. Please allow 1-3 days for processing.</p>
+         <p className='shipping-p'>Shipping takes 7-15 days to arrive. Please allow 1-3 days for processing.</p>
      </div>
   );
 }
