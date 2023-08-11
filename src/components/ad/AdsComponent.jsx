f3e44c90fe4ffbf80fd28c574ab1460b84bc092f
oladunniwaters <waters.oladunni@gmail.com>
2023-08-11 13:32:45 +0100

    ads 4

--- src/components/ad/AdsComponent.jsx
+++ src/components/ad/AdsComponent.jsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react';
 const AdComponent = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+    script.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6917584905087010";
     script.async = true;
     document.head.appendChild(script);
     return () => {
