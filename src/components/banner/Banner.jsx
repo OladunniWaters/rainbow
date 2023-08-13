97f4424962c3d23ae9e1f480756867f524d697d8
oladunniwaters <waters.oladunni@gmail.com>
2023-08-13 23:39:43 +0100

    mudbuster 2

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,7 +5,7 @@ function Banner() {
 
   return (
     <div className="banner" >
-        <p className='banner-p'> Worldwide Shipping on us + FREE 3 TOWELS</p>
+        <p className='banner-p'> Free Worldwide Shipping + FREE 3 TOWELS</p>
     </div>
   );
 }
